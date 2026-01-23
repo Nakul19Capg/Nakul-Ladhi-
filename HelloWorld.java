435bc3e05b6755ee8d5b8d4c34c3b7cbcd94abc8
Ladhi <nakul-someshwar.ladhi@capgemini.com>
2026-01-23 14:24:48 +0530

    Delete HelloWorld.java file

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Welcome to Java programming!");
-        System.out.println("This is a sample Java application pushed to GitHub.");
-    }
-}
