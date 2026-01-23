0954c2af3b60d20690d7a8074777d3474b0d6d2c
Ladhi <nakul-someshwar.ladhi@capgemini.com>
2026-01-23 14:47:13 +0530

    Add HelloWorld.java in feature_mcp_testing branch

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming!");
+        System.out.println("This is a sample Java application in feature_mcp_testing branch.");
+    }
+}
